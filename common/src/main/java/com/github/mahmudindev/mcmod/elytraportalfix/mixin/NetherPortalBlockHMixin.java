d7b9897564e6d11150f5fda3a9136a8527f3aeb6
M. Mahmudin <42290713+mahmudindev@users.noreply.github.com>
2026-01-17 00:13:49 +0700

    Updates to 1.21.2

--- common/src/main/java/com/github/mahmudindev/mcmod/elytraportalfix/mixin/NetherPortalBlockHMixin.java
+++ common/src/main/java/com/github/mahmudindev/mcmod/elytraportalfix/mixin/NetherPortalBlockHMixin.java
@@ -6,12 +6,11 @@ import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ElytraItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.GameRules;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.NetherPortalBlock;
 import org.spongepowered.asm.mixin.Mixin;
 
@@ -26,9 +25,11 @@ public abstract class NetherPortalBlockHMixin {
         if (entity instanceof Player player && player.isFallFlying()) {
             ItemStack itemStack = player.getItemBySlot(EquipmentSlot.CHEST);
 
-            if (itemStack.getItem() == Items.ELYTRA && ElytraItem.isFlyEnabled(itemStack)) {
-                Level level = player.level();
-                GameRules gameRules = level.getGameRules();
+            if (itemStack.getItem() == Items.ELYTRA && LivingEntity.canGlideUsing(
+                    itemStack,
+                    EquipmentSlot.CHEST
+            )) {
+                GameRules gameRules = serverLevel.getGameRules();
 
                 return gameRules.getInt(CustomGameRules.RULE_ELYTRA_FLYING_PORTAL_DELAY);
             }
